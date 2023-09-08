8848bbe2144c66e5213fab00ea26d5f6ab7c96bb
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-09-08 23:09:56 +0530

    whatsapp_clone

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
+import 'package:whatsapp_clone/screens/web_layout_screen.dart';
+import 'package:whatsapp_clone/utils/responsive_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      )
+    );
+  }
+}
