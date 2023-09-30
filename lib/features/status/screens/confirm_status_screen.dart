eeaa8295bd1be59147822c0260adec1923a83297
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-09-30 11:15:52 +0530

    status controller add

--- lib/features/status/screens/confirm_status_screen.dart
+++ lib/features/status/screens/confirm_status_screen.dart
@@ -3,11 +3,17 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/features/status/controller/status_controller.dart';
 
 class ConfirmStatusScreen extends ConsumerWidget {
   static const String routeName = '/confirm-status-screen';
   final File file;
   const ConfirmStatusScreen({super.key, required this.file});
+
+  void addStatus(WidgetRef ref, BuildContext context) {
+    ref.read(statusControllerProvider).addStatus(file, context);
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -19,7 +25,7 @@ class ConfirmStatusScreen extends ConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => addStatus(ref, context),
         child: const Icon(
           Icons.done,
           color: Colors.white,
