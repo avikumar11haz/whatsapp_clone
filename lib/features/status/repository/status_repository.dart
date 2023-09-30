eefdfb1a074c0aa99521b5577e9d0a800d1e6453
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-09-30 10:22:21 +0530

    add status repository

---
+++ lib/features/status/repository/status_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
+
+class StatusRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+  final ProviderRef ref;
+  StatusRepository({
+    required this.firestore,
+    required this.auth,
+    required this.ref,
+  });
+
+  void uploadStatus({
+    required String username,
+    required String profilePic,
+    required String phoneNumber,
+    required File statusImage,
+    required BuildContext context,
+  }) async {
+    try {} catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
+}
