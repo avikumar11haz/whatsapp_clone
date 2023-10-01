40bc15b7c87c91900c8b510e65a861a17a66b1fe
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-10-02 01:31:13 +0530

    add chat controller

---
+++ lib/features/call/controller/chat_controller.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:whatsapp_clone/features/auth/controller/auth_controller.dart';
+import 'package:whatsapp_clone/features/call/repository/call_repository.dart';
+import 'package:whatsapp_clone/models/call.dart';
+
+final callControllerProvider = Provider((ref) {
+  final callRepository = ref.read(callRepositoryProvider);
+  return CallController(
+    callRepository: callRepository,
+    auth: FirebaseAuth.instance,
+    ref: ref,
+  );
+});
+
+class CallController {
+  final CallRepository callRepository;
+  final ProviderRef ref;
+  final FirebaseAuth auth;
+  CallController({
+    required this.callRepository,
+    required this.ref,
+    required this.auth,
+  });
+
+  void makeCall(BuildContext context, String receiverName, String receiverUid,
+      String receiverProfilePic, bool isGroupChat) {
+    ref.read(userDataAuthProvider).whenData((value) {
+      String callId = const Uuid().v1();
+      Call senderCallData = Call(
+        callerId: auth.currentUser!.uid,
+        callerName: value!.name,
+        callerPic: value.profilePic,
+        receiverId: receiverUid,
+        receiverName: receiverName,
+        receiverPic: receiverProfilePic,
+        callId: callId,
+        hasDialled: true,
+      );
+
+      Call receiverCallData = Call(
+        callerId: auth.currentUser!.uid,
+        callerName: value.name,
+        callerPic: value.profilePic,
+        receiverId: receiverUid,
+        receiverName: receiverName,
+        receiverPic: receiverProfilePic,
+        callId: callId,
+        hasDialled: false,
+      );
+
+      callRepository.makeCall(senderCallData, context, receiverCallData);
+    });
+  }
+}
