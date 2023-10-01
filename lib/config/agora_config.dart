a0ae97bf962e1ed49a3f8053fb9742fad76051cd
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-10-02 01:03:39 +0530

    add agora

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = '539ef0bdebdf49d387afc1e543d68c24';
+  static String appcertificate = '539ef0bdebdf49d387afc1e543d68c24';
+}
