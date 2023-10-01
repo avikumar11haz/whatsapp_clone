3353f1106a78115655d1a8d7910036446d18df43
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-10-02 01:17:44 +0530

    add call model and call repository

---
+++ lib/features/call/repository/call_repository.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:whatsapp_clone/common/repositories/common_firebase_storage_respositories.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
+import 'package:whatsapp_clone/models/call.dart';
+import 'package:whatsapp_clone/models/group.dart' as model;
+
+final callRepositoryProvider = Provider(
+  (ref) => CallRepository(
+    firestore: FirebaseFirestore.instance,
+    auth: FirebaseAuth.instance,
+  ),
+);
+
+class CallRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+  CallRepository({
+    required this.firestore,
+    required this.auth,
+  });
+
+  void makeCall(
+    Call senderCallData,
+    BuildContext context,
+    Call receiverCallData,
+  ) async {
+    try {
+      await firestore
+          .collection('call')
+          .doc(senderCallData.callerId)
+          .set(senderCallData.toMap());
+      await firestore
+          .collection('call')
+          .doc(senderCallData.callerId)
+          .set(receiverCallData.toMap());
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
+}
